ec2379ef32f42fc8bbcee994eabee715b32cd928
jellybeerke <je.van.den.broek@sintlucasedu.nl>
2023-10-14 21:44:15 +0200

    some test things

---
+++ src/process_form.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST["name"];
+
+    // Send the email
+    $to = "your@email.com";
+    $subject = "New Form Submission";
+    $message = "Name: " . $name;
+    $headers = "From: webmaster@example.com";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo json_encode(array("success" => true));
+    } else {
+        echo json_encode(array("success" => false));
+    }
+} else {
+    echo json_encode(array("success" => false));
+}
+?>
